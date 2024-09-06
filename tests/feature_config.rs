080a57457cbadcb2f3d80fd65e6e933fce966702
m <m@mcarr.one>
2024-09-06 14:28:48 -0700

    break up feature tests

--- tests/feature_config.rs
+++ tests/feature_config.rs
@@ -71,7 +71,7 @@ fn simple_default_features() {
 }
 
 #[test]
-fn simple_validation_feature_flags() {
+fn comma_separated_features() {
     CargoSemverChecks::new(
         "test_crates/feature_flags_validation/new/",
         "test_crates/feature_flags_validation/old/Cargo.toml",
@@ -89,6 +89,10 @@ fn simple_validation_feature_flags() {
     .for_each(|a| {
         a.success();
     });
+}
+
+#[test]
+fn individually_specified_features() {
     // We repeat the same test, but specify each flag separately,
     // to ensure that both ways can be parsed
     CargoSemverChecks::new(
@@ -146,131 +150,160 @@ fn simple_all_features() {
     });
 }
 
-#[test]
-fn function_moved_only_explicit_features() {
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--only-explicit-features")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+mod only_explicit_features {
+    use super::CargoSemverChecks;
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--only-explicit-features")
-    .add_arg("--baseline-features")
-    .add_arg("C")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn only_explicit_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--only-explicit-features")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--only-explicit-features")
-    .add_arg("--baseline-features")
-    .add_arg("A")
-    .add_arg("--current-features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_baseline_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--only-explicit-features")
+        .add_arg("--baseline-features")
+        .add_arg("C")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--only-explicit-features")
-    .add_arg("--features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.failure();
-    });
+    #[test]
+    fn and_baseline_and_current_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--only-explicit-features")
+        .add_arg("--baseline-features")
+        .add_arg("A")
+        .add_arg("--current-features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--only-explicit-features")
-    .add_arg("--features")
-    .add_arg("A")
-    .add_arg("--features")
-    .add_arg("B")
-    .add_arg("--features")
-    .add_arg("C")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_one_feature() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--only-explicit-features")
+        .add_arg("--features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.failure();
+        });
+    }
+
+    #[test]
+    fn and_many_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--only-explicit-features")
+        .add_arg("--features")
+        .add_arg("A")
+        .add_arg("--features")
+        .add_arg("B")
+        .add_arg("--features")
+        .add_arg("C")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 }
 
-#[test]
-fn function_moved_default_features() {
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.failure();
-    });
+mod default_features {
+    use super::CargoSemverChecks;
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--current-features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn default_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.failure();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.failure();
-    });
+    #[test]
+    fn and_current_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--current-features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--features")
-    .add_arg("B")
-    .add_arg("--current-features")
-    .add_arg("C")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_one_feature() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.failure();
+        });
+    }
+
+    #[test]
+    fn and_many_features() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--features")
+        .add_arg("B")
+        .add_arg("--current-features")
+        .add_arg("C")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 }
 
 #[test]
@@ -300,95 +333,117 @@ fn function_moved_all_features() {
     });
 }
 
-#[test]
-fn default_features_when_default_undefined() {
-    CargoSemverChecks::new(
-        "test_crates/features_no_default/new/",
-        "test_crates/features_no_default/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+mod undefined_default_features {
+    use super::CargoSemverChecks;
+    #[test]
+    fn default_features_when_default_undefined() {
+        CargoSemverChecks::new(
+            "test_crates/features_no_default/new/",
+            "test_crates/features_no_default/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/features_no_default/new/",
-        "test_crates/features_no_default/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--features")
-    .add_arg("A")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_additional_feature() {
+        CargoSemverChecks::new(
+            "test_crates/features_no_default/new/",
+            "test_crates/features_no_default/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--features")
+        .add_arg("A")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/features_no_default/new/",
-        "test_crates/features_no_default/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--baseline-features")
-    .add_arg("A")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_baseline_feature() {
+        CargoSemverChecks::new(
+            "test_crates/features_no_default/new/",
+            "test_crates/features_no_default/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--baseline-features")
+        .add_arg("A")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/features_no_default/new/",
-        "test_crates/features_no_default/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--current-features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+    #[test]
+    fn and_current_feature() {
+        CargoSemverChecks::new(
+            "test_crates/features_no_default/new/",
+            "test_crates/features_no_default/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--current-features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
 
-    CargoSemverChecks::new(
-        "test_crates/features_no_default/new/",
-        "test_crates/features_no_default/old/Cargo.toml",
-    )
-    .add_arg("--default-features")
-    .add_arg("--features")
-    .add_arg("B")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.failure();
-    });
+    #[test]
+    fn and_other_feature() {
+        CargoSemverChecks::new(
+            "test_crates/features_no_default/new/",
+            "test_crates/features_no_default/old/Cargo.toml",
+        )
+        .add_arg("--default-features")
+        .add_arg("--features")
+        .add_arg("B")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.failure();
+        });
+    }
 }
 
-#[test]
-fn feature_does_not_exist() {
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--features")
-    .add_arg("new_feature")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.success();
-    });
+mod missing_feature {
+    use super::CargoSemverChecks;
 
-    CargoSemverChecks::new(
-        "test_crates/function_feature_changed/new/",
-        "test_crates/function_feature_changed/old/Cargo.toml",
-    )
-    .add_arg("--features")
-    .add_arg("feature_to_be_removed")
-    .run_all()
-    .into_iter()
-    .for_each(|a| {
-        a.failure();
-    });
+    #[test]
+    fn in_baseline() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--features")
+        .add_arg("new_feature")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.success();
+        });
+    }
+
+    #[test]
+    fn in_current() {
+        CargoSemverChecks::new(
+            "test_crates/function_feature_changed/new/",
+            "test_crates/function_feature_changed/old/Cargo.toml",
+        )
+        .add_arg("--features")
+        .add_arg("feature_to_be_removed")
+        .run_all()
+        .into_iter()
+        .for_each(|a| {
+            a.failure();
+        });
+    }
 }
